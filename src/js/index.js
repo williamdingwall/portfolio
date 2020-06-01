0637937ad05d87a3a53d9ca0d2c89dd5920f3929
William Dingwall <dev@williamdingwall.com>
2020-06-01 12:21:49 -0600

    adding photo swipe for an image lightbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 
 
-// conditionally load livereload based on host
+// conditionally load livereload & GA based on host
 
 var hostName = window.location.hostname;
+
+if (hostName === 'williamdingwall.com') {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-163835667-1');
+}
 
 if (hostName === 'localhost') {
   var livereloadScript = document.createElement('script');
